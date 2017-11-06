958e3ea4cc94efa60f24d75dfeb0b2898269bcf5
Rishabh Gupta <rishabhgupta@Rishabhs-MacBook-Air.local>
2017-11-06 21:07:26 +0530

    adds seller side apis

--- routes/products.js
+++ routes/products.js
@@ -2,12 +2,14 @@ var express = require('express');
 var router = express.Router();
 var mongodb = require('mongodb');
 
+var dbName = "customers";
+
 /*
  * GET productlist.
  */
 router.get('/productlist', function(req, res) {
     var db = req.db;
-    var collection = db.get('customers');
+    var collection = db.get(dbName);
     collection.find({},{},function(e,docs){
         res.json(docs);
     });
@@ -18,9 +20,21 @@ router.get('/productlist', function(req, res) {
  */
 router.get('/getproduct/:id', function(req, res) {
     var db = req.db;
-    var collection = db.get('customers');
+    var collection = db.get(dbName);
     var ObjectID = req.ObjectID;
     var query = { _id: new ObjectID(req.params.id) };
+    collection.findOne(query, function(e,docs){
+        res.json(docs);
+    });
+});
+
+/*
+ * GET product list from seller address.
+ */
+router.get('/sellerproductlist/:sellerAddress', function(req, res) {
+    var db = req.db;
+    var collection = db.get(dbName);
+    var query = { sellerAddress: req.params.sellerAddress };
     collection.find(query, function(e,docs){
         res.json(docs);
     });
@@ -31,7 +45,8 @@ router.get('/getproduct/:id', function(req, res) {
  */
 router.post('/addproduct', function(req, res) {
     var db = req.db;
-    var collection = db.get('customers');
+    var collection = db.get(dbName);
+    console.log(req.body);
     collection.insert(req.body, function(err, result){
         res.send(
             (err === null) ? { 
@@ -48,13 +63,33 @@ router.post('/addproduct', function(req, res) {
 router.put('/updatestatus', function(req, res) {
     var db = req.db;
     var ObjectID = req.ObjectID;
-    var collection = db.get('customers');
+    var collection = db.get(dbName);
     var query = { _id: new ObjectID(req.body._id) };
-    var newvalues = { $set: { status: req.body.status } };
+    var newvalues;
+    if(req.body.status == "Confirmed")
+        newvalues= { $set: { status: req.body.status, buyerAddress: req.body.buyerAddress } };
+    else
+        newvalues = { $set: { status: req.body.status } };
     collection.update(query, newvalues, function(err, result) {
         res.send(
             (err === null) ? { msg: 'Status of product changed successfully.' } : { msg: err }
         );
+    });
+});
+
+/*
+ * DELETE to delete product.
+ */
+router.delete('/deleteproduct/:sellerAddress/:id', function(req, res) {
+    var db = req.db;
+    var collection = db.get(dbName);
+    var ObjectID = req.ObjectID;
+    var query = { 
+        _id: new ObjectID(req.params.id), 
+        sellerAddress: req.params.sellerAddress
+    };
+    collection.remove(query, function(err) {
+        res.send((err === null) ? { msg: 'Product deleted successfully.' } : { msg:'error: ' + err });
     });
 });
 
